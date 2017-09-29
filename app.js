f7a3929fee8d1c4372a2e310dd28adee246ea58b
squid <squid@squiddeMacBook-Pro.local>
2017-09-29 17:10:46 +0800

    fix router

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const serve = require('koa-static');
 const session = require('koa-session');
-const KeyGrip = require("keygrip")
 var bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
@@ -11,13 +10,10 @@ const router = new Router();
 app.use(bodyParser());
 
 
-app.keys = new KeyGrip(['im a newer secret', 'i like turtle'], 'sha256');
+app.keys = ['im a newer secret', 'i like turtle'];
 
 const CONFIG = {
   key: 'koa_session',
-  /** (number || 'session') maxAge in ms (default is 1 days) */
-  /** 'session' will result in a cookie that expires when session/browser is closed */
-  /** Warning: If a session cookie is stolen, this cookie will never expire */
   maxAge: 86400000,
   overwrite: true, /** (boolean) can overwrite or not (default true) */
   httpOnly: true, /** (boolean) httpOnly or not (default true) */
--- config/routers.js
+++ config/routers.js
@@ -1,24 +1,35 @@
+const fs = require('fs');
 
 // 要继承的类必须写前面
 const routers = {
 
-  '/':['get','application','index'],
-  // '/':['get','user','index'],
+  // '/':['get','application','index'],
+  '/':['get','user','index'],
   '/user':['post','user','index'],
   '/user/:id':['get','user','show'],
-
 }
+
+
+
+var preload_conroller = ['application_controller.js']
+
+var controllers  = fs.readdirSync('./controllers/');
+controllers = preload_conroller.concat(controllers)
+
+controllers.map(function (c_name) {
+  let controller = require('./../controllers/'+c_name);
+  c_name = c_name.replace('_c',"C")
+  c_name = c_name.replace('.js',"")
+  if(!global[c_name]){
+    global[c_name] = controller;
+  }
+})
+
 
 module.exports = function (router) {
   for(var r in routers){
     var r_self = routers[r]
-    var r_f_c = require('./../controllers/'+r_self[1]+'_controller.js')
-
-    var gl_name = r_self[1]+"Controller"
-
-    if(!global[gl_name]){
-      global[gl_name] = r_f_c
-    }
+    var r_f_c = global[r_self[1]+'Controller']
 
     var r_fn = new r_f_c()
     router[r_self[0]](r, r_fn[r_self[2]]);
