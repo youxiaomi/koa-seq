6913cd54f079a0ab6ab71a012b25f9a6af93075a
squid <squid@squiddeMacBook-Pro.local>
2017-10-21 13:17:50 +0800

    rm static

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ global.ROOT_DIR_PUBLIC = __dirname+'/public';
 app.keys = ['mima'];
 const CONFIG = {
   key: 'koa_session',
-  maxAge: 86400000,
+  maxAge: 1000*60*60*24*10,
   overwrite: true, /** (boolean) can overwrite or not (default true) */
   httpOnly: true, /** (boolean) httpOnly or not (default true) */
   signed: true, /** (boolean) signed or not (default true) */
-  rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. default is false **/
+  rolling: true, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. default is false **/
 };
 
 app.use(session(CONFIG, app));
